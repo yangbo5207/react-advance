a8e7f9f137eaad12425679627e9515cce1db409d
阳波 <1832064870@qq.com>
2018-11-29 16:07:46 +0800

    fix: button 点击

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,19 +4,45 @@ import './style.scss';
 
 class Button extends Component {
   static defaultProps = {
-    loading: false,
-    type: 'default', // default warning primary 
+    type: 'default', // default warning primary
+  }
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = (e) => {
+    console.log('xxxx');
+    const { onClick } = this.props;
+    this.setState({
+      clicked: true
+    })
+
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.setState({ clicked: false })
+    }, 500);
+
+    onClick && onClick(e);
   }
 
   render() {
     const { type, ghost, className, children, ...other } = this.props;
+    const { clicked } = this.state;
+
+    console.log(clicked);
 
     const cls = classnames('button', {
       [`button-${type}`]: true,
-      'button-ghost': ghost
+      'button-ghost': ghost,
+      'button-clicked': clicked
     }, className);
     return (
-      <button className={cls}>
+      <button 
+        className={cls}
+        onClick={this.clickHandler}
+        {...other}
+      >
         {children}
       </button>
     )
